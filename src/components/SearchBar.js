3f046114fff2bea9154ec61f0e6834d91291fd83
Vladimir Gubarevich <vladimir.hubarevich@intexsoft.by>
2020-04-07 16:33:48 +0300

    Update component SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
-import Input from '../components/Input';
+
 import Select from '../components/Select';
 import { formStyles } from '../material.styles';
-
 import { countries } from '../enum/country.enums';
 
 export default function SearchBar(props) {
@@ -17,12 +17,12 @@ export default function SearchBar(props) {
 	const [category, setCategory] = useState('');
 
 	const location = {
-		country,
-		city
+		country: country,
+		city: city
 	}
 
 	const filter = {
-		keyword, 
+		keyword: keyword,
 		classification: category
 	}
 
@@ -30,39 +30,49 @@ export default function SearchBar(props) {
 		button.handler(location, filter);
 	}
 
+	useEffect(() => {
+		countryInput && setCountry(countryInput.value);
+		cityInput && setCity(cityInput.value);
+		keywordInput && setKeyword(keywordInput.value);
+		categoryInput && setCategory(categoryInput.value);
+		 // eslint-disable-next-line
+	}, [])
+
 	return (
 		<form className="search-bar">
 			{countryInput && <Select
 				items={countries}
-				label={countryInput.label || 'Страна'}
-				value={countryInput.value || country}
 				onchange={setCountry}
-				id={'countrySelect'}
+				value={countryInput.value || country}
+				label={countryInput.label || 'Страна'}
+				inputProps={{ 'data-testid': "countries" }}
 			/>}
 			{cityInput &&
 				<FormControl className={classes.formControl}>
-					<Input
+					<TextField
+						value={city}
 						label={cityInput.label || 'Город'}
-						inputHandler={setCity}
-						value={cityInput.value || city}
-						id={'cityInput'}
+						onChange={e => setCity(e.target.value)}
+						inputProps={{ 'data-testid': "cityInput" }}
 					/>
 				</FormControl>
 			}
 			{keywordInput &&
 				<FormControl className={classes.formControl}>
-					<Input
-						value={keywordInput.value || keyword}
-						inputHandler={setKeyword}
+					<TextField
+						value={keyword}
+						onChange={e => setKeyword(e.target.value)}
+						inputProps={{ 'data-testid': "keywordInput" }}
 						label={keywordInput.label || 'Ключевое слово'}
 					/>
 				</FormControl>}
 			{categoryInput &&
 				<Select
-					label={categoryInput.label || 'Категория'}
-					items={categoryInput.items}
 					onchange={setCategory}
+					items={categoryInput.items}
 					value={categoryInput.value || category}
+					label={categoryInput.label || 'Категория'}
+					inputProps={{ 'data-testid': "category" }}
 				/>}
 			{button && <Button className={classes.button} variant="contained" onClick={buttonHandler} color="primary">
 				{button.name || 'Поиск'}
